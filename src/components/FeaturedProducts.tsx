17cd00c50ca54a7ac5cf7a41600afdcab611478d
RemeskoXD <ludvikremesekwork@gmail.com>
2026-06-21 15:29:01 +0200

    Strip HTML tags from FeaturedProducts description

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -151,7 +151,7 @@ export default function FeaturedProducts() {
                     {product.title}
                   </h3>
                   <p className="text-sm text-gray-500 mb-8 line-clamp-2 leading-relaxed">
-                    {product.desc}
+                    {product.desc ? product.desc.replace(/<[^>]+>/g, '') : ''}
                   </p>
                   
                   {/* Pricing & Add to cart */}
